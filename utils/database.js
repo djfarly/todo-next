731108a4002905f33286a4179d83dd6384e85d49
Jan Willem Henckel <jan@mpfilm.de>
2021-10-21 09:33:32 +0200

    use .env variable

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,3 @@
 import { MongoClient } from 'mongodb';
 
-const uri = 'mongodb://localhost:27017/';
-export const client = new MongoClient(uri);
+export const client = new MongoClient(process.env.MONGODB_URL);
